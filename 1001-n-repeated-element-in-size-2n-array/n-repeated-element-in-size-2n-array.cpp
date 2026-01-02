1efeba602229a3b1a7f46e4a6e94882be58d3a8d
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-01-02 21:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (87.61%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,13 +1,28 @@
-class Solution {
-public:
-    int repeatedNTimes(vector<int>& nums) {
-        int n = nums.size()/2;
+// class Solution {
+// public:
+//     int repeatedNTimes(vector<int>& nums) {
+//         int n = nums.size()/2;
 
-        unordered_map<int,int>mpp;
-        for(int &num : nums){
-            mpp[num]++;
+//         unordered_map<int,int>mpp;
+//         for(int &num : nums){
+//             mpp[num]++;
             
-            if(mpp[num]==n)return num;
+//             if(mpp[num]==n)return num;
+//         }
+// return -1;    }
+// };
+//PigeonHole Principle :- it states that if you have more items ("pigeons") than containers ("pigeonholes"), at least one container must hold more than one item.
+class Solution{
+    public:
+    int repeatedNTimes(vector<int>&nums){
+
+        int n = nums.size();
+
+        for(int i=0;i<n-2;i++){
+            if(nums[i]==nums[i+1]||nums[i]==nums[i+2]){
+                return nums[i];
+            }
         }
-return -1;    }
+        
+  return nums[n-1];  }
 };
