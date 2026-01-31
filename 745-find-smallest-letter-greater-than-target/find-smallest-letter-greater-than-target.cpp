87442c11138c47c9a2505b2aa98bc1099b177f54
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-01-31 23:06:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (62.61%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l = 0;
+        int r = letters.size()-1;
+        int mid;
+        char ans = letters[0]; 
+        while(l<=r){
+            mid = l +(r-l/2); 
+
+            if(letters[mid]>target){
+                ans = letters[mid];
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+
+        }
+
+    return ans;}
+};
