cd697f97b4661aabe1154e1038d28984d41f91e3
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-03-08 17:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (55.15%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -23,22 +23,36 @@ public:
 // return "000";
 //     }
 
-//Approach 2 By using the decimal representation
- string findDifferentBinaryString(vector<string>& nums) {
-    int n = nums.size();
+// //Approach 2 By using the decimal representation
+//  string findDifferentBinaryString(vector<string>& nums) {
+//     int n = nums.size();
      
-     unordered_set<int>st;
-    for(string&num:nums){
-        st.insert(stoi(num,0,2));//this directly converts binary str. to its decimal representation
+//      unordered_set<int>st;
+//     for(string&num:nums){
+//         st.insert(stoi(num,0,2));//this directly converts binary str. to its decimal representation
 
-    }
-  string bin = "";
-    for(int num=0;num<=n;num++){
-        if(!st.count(num)){
-           bin = bitset<16>(num).to_string();
-            return bin.substr(16-n,n);
-        }
+//     }
+//   string bin = "";
+//     for(int num=0;num<=n;num++){
+//         if(!st.count(num)){
+//            bin = bitset<16>(num).to_string();
+//             return bin.substr(16-n,n);
+//         }
+//     }
+
+//  return "0000";}
+
+//3rd and most fasinating approach 
+// in this approach what we will do is that we through away each input from results equation by flipping one of the bit in it which will eventually result in our Final ans  
+
+//This trick comes from a famous mathematical proof called Cantor’s Diagonal Argument.
+ string findDifferentBinaryString(vector<string>& nums) {
+    string res="";
+
+    for(int i=0;i<nums.size();i++){
+        int bit = nums[i][i];
+        res+=(bit=='0')?'1':'0';
     }
 
- return "0000";}
+ return res;}
 };
