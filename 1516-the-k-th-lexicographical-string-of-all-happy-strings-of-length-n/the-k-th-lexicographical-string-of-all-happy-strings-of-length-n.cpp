e26a80c2b3e0888017e95d710de4563356e2dbc6
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-03-14 14:30:54 +0530

    Time: 3 ms (59.10%) | Memory: 8.9 MB (64.49%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,29 +1,70 @@
+// class Solution {
+// public:
+//     string getHappyString(int n, int k) {
+//           //Brute Force;
+//            string curr ="";
+//         string res = "";
+//         vector<string>str;
+
+//         generateString(curr,n,str);
+
+//         if(str.size()<k)return "";
+
+//         return str[k-1];
+
+        
+//     }
+//     void generateString(string& curr,int n,vector<string>&str){
+//         if(curr.size()==n){
+//            str.push_back(curr);
+//             return;
+//         }
+        
+//         for(char ch = 'a';ch<='c';ch++){
+//             if(curr.size()>0 && curr.back()==ch)continue;
+//             curr = curr+ch;
+//             generateString(curr,n,str);
+
+//             curr.pop_back();//backtrack
+             
+//         }
+
+
+//     }
+// };
+//Optimized
 class Solution {
 public:
     string getHappyString(int n, int k) {
           //Brute Force;
            string curr ="";
         string res = "";
-        vector<string>str;
+        
+        int index=0;//no happy strings
+        generateString(curr,n,res,index,k);
 
-        generateString(curr,n,str);
-
-        if(str.size()<k)return "";
-
-        return str[k-1];
+      return res;
 
         
     }
-    void generateString(string& curr,int n,vector<string>&str){
+    void generateString(string& curr,int n,string& res ,int &index,int k){
         if(curr.size()==n){
-           str.push_back(curr);
-            return;
+           index++;
+
+           if(index==k){
+            res=curr;
+           }
+           return;
+           
         }
         
         for(char ch = 'a';ch<='c';ch++){
             if(curr.size()>0 && curr.back()==ch)continue;
             curr = curr+ch;
-            generateString(curr,n,str);
+            generateString(curr,n,res,index,k);
+            
+            // If the result is found, stop further processing
+            if (res != "") return;
 
             curr.pop_back();//backtrack
              
