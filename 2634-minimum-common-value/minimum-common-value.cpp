236663dd99bb50b62992c79205b892b6534a5527
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-05-19 11:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (58.81%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int>st(nums1.begin(),nums1.end());
-        for(int&n:nums2){
-            if(st.count(n)){
-                return n;
-            }
-        }
-    return -1;}
+    int getCommon(vector<int>& arr1, vector<int>& arr2) {
+        int n = arr1.size(),m=arr2.size();
+        int i=0,j=0;
+    while(i<n && j<m){
+        if(arr1[i]==arr2[j]){
+            return arr1[i];
+          }
+        else if(arr1[i]<arr2[j])i++;
+        else j++;
+    }
+   return -1; }
 };
