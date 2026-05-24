ffe54d2641da1d1efe9f40fff0f00f48a47d63b7
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-05-24 14:34:54 +0530

    Time: 23 ms (22.32%) | Memory: 19.4 MB (30.28%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+vector<int>dp,nums;
+int n;
+
+     int solve(int currIdx , int d){
+        if(currIdx>=n)return 0;
+
+        if(dp[currIdx]!=0)return dp[currIdx];
+
+        int jumps = 1;
+        
+        for(int x=1;x<=d;x++){
+        //1st condition 
+        if((currIdx+x)<n && nums[currIdx]>nums[currIdx+x] ){
+           jumps = max(jumps,1+solve(currIdx+x,d));
+        }
+        else break;
+        }
+
+        for(int x=1;x<=d;x++){
+        //2st condition 
+        if((currIdx-x)>=0 && nums[currIdx]>nums[currIdx-x] ){
+           jumps = max(jumps,1+solve(currIdx-x,d));
+        }
+        else break;
+        }
+        return dp[currIdx] = jumps;
+     }
+    int maxJumps(vector<int>& arr, int d) {
+      n=arr.size();
+      nums=arr;
+      dp.assign(n,0);
+      int ans =0 ;
+      for(int i=0;i<n;i++){
+       ans = max(ans,solve(i,d));
+    
+      }  
+      return ans;
+    }
+};
