0f2ea84202d408f1d904a28760a5577a2d913c6a
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-05-26 12:31:03 +0530

    Time: 870 ms (20.94%) | Memory: 344.6 MB (26.93%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -1,44 +1,37 @@
 class Solution {
-public:
-int n,K;
-typedef long long ll;
-long long solve(int idx,int trans,int state,vector<int>&prices,vector<vector<vector<ll>>>&dp){
-   if (idx == n) {
-            if (state == 0) return 0;
-            return -1e9;
-        }
-
-    if(dp[idx][trans][state]!=-1){
-        return dp[idx][trans][state];
+public:typedef long long ll;
+int n,maxTrans;
+ ll solve(int i,int trans,int state,vector<vector<vector<ll>>>&dp,vector<int>&prices){
+    if(i==n){
+        return state==0?0:-1e9;
     }
 
-    ll ans = solve(idx+1,trans,state,prices,dp);//no position
+    if(dp[i][trans][state]!=-1)return dp[i][trans][state];
+    ll ans = solve(i+1,trans,state,dp,prices);
 
-    if(state == 0){
-        //try both longHand(i.e.holding)and shorthand (shortselling)
-         
-         ans = max(ans,solve(idx+1,trans,1,prices,dp)-prices[idx]);//longhand
-         ans = max(ans,solve(idx+1,trans,2,prices,dp)+prices[idx]);//shorthand
 
+    if(state==0){
+        //trying both
+        ans = max(ans,solve(i+1,trans,1,dp,prices)-prices[i]);//longhand
+        ans = max(ans,solve(i+1,trans,2,dp,prices)+prices[i]);//shorthand
     }
-    else if(state==1 && trans<K){
-        //longHand state
-        //selling the holded stock
-        ans = max(ans,solve(idx+1,trans+1,0,prices,dp)+prices[idx]);
+    else if(state==1 && trans<maxTrans){
+        //selling holded stock
+        ans = max(ans,solve(i+1,trans+1,0,dp,prices)+prices[i]);
     }
-    else if(state==2 && trans<K){
-        //shortHand state
-        //buying  the shortselled stock
-        ans = max(ans,solve(idx+1,trans+1,0,prices,dp)-prices[idx]);
+    else if(state==2 && trans<maxTrans){
+        //selling holded stock
+        ans = max(ans,solve(i+1,trans+1,0,dp,prices)-prices[i]);
     }
- return   dp[idx][trans][state]=ans;
 
-}
+    return dp[i][trans][state] = ans;
+ }
 
     long long maximumProfit(vector<int>& prices, int k) {
         n = prices.size();
-        K=k;
-        vector<vector<vector<ll>>>dp(n,vector<vector<ll>>(K+1,vector<ll>(3,-1)));
-        return solve(0,0,0,prices,dp);
+        vector<vector<vector<ll>>>dp(n,vector<vector<ll>>(k+1,vector<ll>(3,-1)));
+        maxTrans=k;
+
+       return solve(0,0,0,dp,prices);
     }
 };
