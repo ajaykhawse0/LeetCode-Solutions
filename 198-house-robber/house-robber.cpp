1cc46d5aa147cc28577ec461a1e91d554c22b268
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-06-03 20:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (67.02%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
+vector<int>dp;
+int solve(int idx,vector<int>&nums){
+    if(idx==0)return nums[0];
+    if(idx<0)return 0;
+    if(dp[idx]!=-1)return dp[idx];
+
+    int take = nums[idx] + solve(idx-2,nums);
+    int skip =  solve(idx-1,nums);
+
+    return dp[idx] = max(skip,take);
+
+}
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1)return nums[0];
+        dp.assign(n,-1);
 
-        vector<int>dp(n);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);//because robber can rob only one of adjacent house
-
-        for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);//rob the current house or the adjacent one which one gives more profit
-        }
-return dp[n-1];
+        return solve(n-1,nums);
+        
     }
 };
+//BOTTOM UP => TABULATION
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n==1)return nums[0];
+
+//         vector<int>dp(n);
+//         dp[0]=nums[0];
+//         dp[1]=max(nums[0],nums[1]);//because robber can rob only one of adjacent house
+
+//         for(int i=2;i<n;i++){
+//             dp[i]=max(dp[i-1],nums[i]+dp[i-2]);//rob the current house or the adjacent one which one gives more profit
+//         }
+// return dp[n-1];
+//     }
+// };
