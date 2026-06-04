c3ab6576e8b61f656a1b6c26294227995a194de1
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-06-04 23:23:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (29.76%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -6,15 +6,24 @@ public:
             freq[num]++;
         }
 
-        priority_queue<pair<int,int>>pq;//maxHeap {freq,num};
+        // priority_queue<pair<int,int>>pq;//maxHeap {freq,num};
+        set<pair<int,int>>st;
 
         for(auto & it:freq){
-            pq.push({it.second,it.first});
+            // pq.push({it.second,it.first});
+            st.insert({it.second,it.first});
+            if(st.size()>k)st.erase(st.begin());
         }
-        vector<int>ans(k);
-        for(int i=0;i<k;i++){
-            ans[i]=pq.top().second;
-            pq.pop();
+        vector<int>ans;
+        // vector<int>ans(k);
+        // for(int i=0;i<k;i++){
+        //     ans[i]=pq.top().second;
+        //     pq.pop();
+        // }
+        for(auto&it:st){
+            ans.push_back(it.second);
+            
         }
+        reverse(ans.begin(),ans.end());
     return ans;}
 };
