d06ea7f0489f19827145c131092835218eca6d13
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-06-08 01:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (88.80%) - LeetSync

---
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findValidElements(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)return nums;
+        vector<int>ans;
+
+        for(int i=0;i<n;i++){
+        bool leftGreater=true;
+        bool rightGreater=true;
+
+        for(int j=0;j<i;j++){
+            if(nums[i]<=nums[j]){
+                leftGreater=false;
+                break;
+            }
+        }
+        for(int j=i+1;j<n;j++){
+                   if(nums[i]<=nums[j]){
+                rightGreater=false;
+                break;
+            }
+        }
+        if(rightGreater || leftGreater){
+            ans.push_back(nums[i]);
+        }
+        }
+
+    return ans;}
+};
