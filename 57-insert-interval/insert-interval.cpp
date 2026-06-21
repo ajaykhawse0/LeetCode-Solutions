a3a0c3745d3f42750d81a672cb031e991c82b5b6
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-06-21 09:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (46.66%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -23,11 +23,40 @@ return results;
         //1.Simply insert the newInterval at the very end And then call the helper
         //mergeInterval function that will take care of the overalpping ones 
         
-        intervals.push_back(newInterval);
+        // intervals.push_back(newInterval);
 
-        return mergeIntervals(intervals);
+        // return mergeIntervals(intervals);
 
 
         //2.Optimal approach that is to handle overlapping interval on the go;
+
+        int n = intervals.size();
+        vector<vector<int>>result;
+        int i = 0;
+        //traverse till the end of an interval is lesser than the newInterval
+        while(i<n && intervals[i][1]<newInterval[0]){
+            result.push_back(intervals[i]);
+            i++;
+        }
+        //now it is a case where there can be overlapping or a place for newInterval;
+
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min(newInterval[0],intervals[i][0]);
+            newInterval[1] = max(newInterval[1],intervals[i][1]);
+
+            i++;
+        }
+
+        result.push_back(newInterval);
+        
+        //insert remaining interval
+
+        while(i<n){
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+return result;
+
     }
 };
