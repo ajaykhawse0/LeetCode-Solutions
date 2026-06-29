439bdeaf6b11139247a0cbaaa4d89410c2566765
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-06-29 16:40:17 +0530

    Time: 3 ms (94.40%) | Memory: 57.2 MB (4.48%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -2,14 +2,34 @@ class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         //brute force
-        sort(arr.begin(),arr.end());
-        int res = 1;
+//         sort(arr.begin(),arr.end());
+//         int res = 1;
 
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]>=res+1){
-               res++;
-            }
-        }
+//         for(int i=1;i<arr.size();i++){
+//             if(arr[i]>=res+1){
+//                res++;
+//             }
+//         }
+// return res;
+
+//optimal
+
+int n = arr.size();
+vector<int>freq(n+1,0);
+
+for(int i=0;i<n;i++){
+    freq[min(arr[i],n)]++;
+}
+
+int res = 1;
+
+for(int i=2;i<=n;i++){
+
+    res = min(res+freq[i],i);
+
+}
+   
 return res;
+
     }
 };
