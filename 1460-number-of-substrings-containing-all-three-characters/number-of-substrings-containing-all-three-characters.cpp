d3a90b1822f6ee6018a6a05a396160ab73a4c7b4
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-06-30 17:42:13 +0530

    Time: 4 ms (79.36%) | Memory: 10.9 MB (81.80%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-     //khisakti hue window(sliding window)
+    // int numberOfSubstrings(string s) {
+    //  //khisakti hue window(sliding window)
 
-     int l = 0;
-     int r = 0;
-     int n = s.size();
-     int freq[3]={0};
-     int ans = 0;
+    //  int l = 0;
+    //  int r = 0;
+    //  int n = s.size();
+    //  int freq[3]={0};
+    //  int ans = 0;
 
-     while(r<n){
+    //  while(r<n){
        
-       freq[s[r]-'a']++;
+    //    freq[s[r]-'a']++;
 
-       while(freq[0]>=1 && freq[1]>=1 && freq[2]>=1){
-            ans += n-r;
-            freq[s[l]-'a']--;
-            l++;
-       }
-       r++;
+    //    while(freq[0]>=1 && freq[1]>=1 && freq[2]>=1){
+    //         ans += n-r;
+    //         freq[s[l]-'a']--;
+    //         l++;
+    //    }
+    //    r++;
   
 
-     }
+    //  }
+
+    // return ans;}
+    int numberOfSubstrings(string s) {
+    
+     int n = s.size();
+     int a = -1; 
+     int b = -1; 
+     int c = -1; 
+     int ans = 0;
+
+    for(int i=0;i<n;i++){
+        if(s[i]=='a'){
+            a = i;
+        }
+        else if(s[i]=='b'){
+            b = i;
+        }
+        else{
+            c = i;
+        }
+
+        if(a!=-1 && b!=-1 && c!=-1){
+            ans += min({a,b,c})+1;
+        }
+
+    }
+  
+
+     
 
     return ans;}
 };
