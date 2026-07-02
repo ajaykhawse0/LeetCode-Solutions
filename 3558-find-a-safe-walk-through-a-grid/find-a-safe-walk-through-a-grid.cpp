487f9685e1aa48542bbfd83c6a42bc447bb6ef9e
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-07-02 12:21:05 +0530

    Time: 12 ms (79.97%) | Memory: 31.2 MB (67.95%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -1,51 +1,39 @@
 class Solution {
-public:typedef pair<int,pair<int,int>>P;
-vector<vector<int>>dirs={{1,0},{0,1},{-1,0},{0,-1}};
+public:
+typedef pair<int,pair<int,int>>PP;
+    vector<vector<int>>dirs = {{1,0},{0,1},{-1,0},{0,-1}};
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size();
         int n = grid[0].size();
-
-        priority_queue<P>q;
-        vector<vector<int>>best(m,vector<int>(n,-1));
-        
         int startHealth = health-grid[0][0];
         if(startHealth<=0)return false;
-        q.push({startHealth,{0,0}});
-        best[0][0]=startHealth;
-        
+        priority_queue<PP>pq;
+        pq.push({startHealth,{0,0}});
+        vector<vector<int>>best(m,vector<int>(n,0));
+        best[0][0] = startHealth;
 
-        while(!q.empty()){
-            int remainHealth = q.top().first;
-            int row = q.top().second.first;
-            int col = q.top().second.second;
-            
-            q.pop();
-            
-            if(row==m-1 && col == n-1 ){
-               
-                    cout<<best[row][col];
-                    return true;}
+        while(!pq.empty()){
+            auto[row,col] = pq.top().second;
+            int currHealth = pq.top().first;
+            pq.pop();
 
-            
-            if(remainHealth<best[row][col])continue;
-            
+            if(row == m-1 && col == n-1)return true;
+            if(currHealth<best[row][col])continue;
+
             for(auto&dir:dirs){
                 int nr = row+dir[0];
                 int nc = col+dir[1];
 
-                if(nr<0 || nr>=m || nc<0 || nc>=n )continue;
-
-                   int newHealth = remainHealth - grid[nr][nc];
-
-                   if(newHealth>0 && newHealth>best[nr][nc]){
-                    best[nr][nc]=newHealth;
-                    q.push({newHealth,{nr,nc}});
-                   }
-                  
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n){
+                int newHealth = currHealth - grid[nr][nc];
                 
-            }
+                if(newHealth > 0 && best[nr][nc] < newHealth ){
+                    best[nr][nc] = newHealth;
 
-            
-             }
-   return 0; }
+                    pq.push({newHealth,{nr,nc}}); 
+                }
+                }
+            }
+        }
+  return false;  }
 };
