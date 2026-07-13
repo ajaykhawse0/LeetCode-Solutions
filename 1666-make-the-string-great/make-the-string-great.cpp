8ac88d0b4ca7d245c7e3248a20c2be1359ec7a33
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-07-13 18:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (69.80%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     string makeGood(string s) {
-      //diff betn lowercase and uppercase is 32 in ascii it will help us
+        // diff betn lowercase and uppercase is 32 in ascii it will help us
 
-      string ans  = "";
+        string ans = "";
 
-      for(char c:s){
-        if(!ans.empty()){
-            if(islower(c) && ans.back()+32 == c){
-                ans.pop_back();
-            }
-            else if( ans.back()-32 == c){
-                ans.pop_back();
-            }
-            else{
+        for (char c : s) {
+            if (!ans.empty() && abs(ans.back()-c) == 32) {
+               ans.pop_back();
+
+            } else {
                 ans.push_back(c);
             }
         }
-            else{
-                ans.push_back(c);
-            }
-      }   
- return ans;   }
+        return ans;
+    }
 };
