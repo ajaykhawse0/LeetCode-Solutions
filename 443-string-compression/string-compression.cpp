5455b7bad944f83d945c11c335180c082d981f50
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-07-15 18:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (47.89%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -2,38 +2,27 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int n = chars.size();
-        if(n==1)return 1;
-        string s = "";
-        int currCnt = 1;
-        char currChar='\0';
-        for(int i=0 ; i<n-1;i++){
-          
-           if(chars[i] == chars[i+1]){
-            currCnt++;
-            currChar = chars[i];
-           }
-           else{
-            s+=currChar=='\0'?chars[i]:currChar;
-            if(currCnt>1){
-                s+=to_string(currCnt);
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        char letter='\0';
+
+        while(i<n){
+            letter = chars[i];
+            count = 1;
+            i++;
+            while(i<n && chars[i]==letter){
+                count++;
+                i++; 
             }
-            currCnt=1;
-            currChar='\0';
-           }
-        }
-        if(currChar!='\0'){
-            s+=currChar;
-            if(currCnt>1){
-                s+=to_string(currCnt);
+            chars[j++] = letter;
+
+            if(count>1){
+                for(char c:to_string(count)){
+                    chars[j++] = c;
+                }
             }
         }
-        if(chars[n-1]!=chars[n-2]){
-            s += chars[n-1];
-        }
-        chars.resize(s.size());
-        for(int i=0;i<s.size();i++){
-            chars[i]=s[i];
-        }
-        
-    return chars.size();}
+        chars.resize(j);
+ return j ;  }
 };
