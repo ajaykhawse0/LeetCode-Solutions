cac809db78f8ccea256099d8ec2d8b5e6f79640c
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-07-16 00:23:51 +0530

    Time: 483 ms (11.23%) | Memory: 101.1 MB (14.36%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -10,39 +10,63 @@
  * right(right) {}
  * };
  */
+/*
 class Solution {
 public:
-    int zigZag(TreeNode* node, int dir, int cnt) {
+
+  //Brute force
+   int zigZag(TreeNode* node, int dir, int cnt) {
+       if (!node)
+           return cnt ;
+       cnt++;
+       if (dir == 0) { // last move was left
+           return zigZag(node->right, 1, cnt);
+       } else { // last move was right
+           return zigZag(node->left, 0, cnt);
+       }
+   }
+   int longestZigZag(TreeNode* root) {
+       if (!root)
+           return 0;
+       stack<TreeNode*> st;
+       int ans = 0;
+       st.push(root);
+
+       while (!st.empty()) {
+           auto node = st.top();
+           st.pop();
+
+           ans =
+               max({ans, zigZag(node->left, 0, 0), zigZag(node->right, 1, 0)});
+
+           if (node->left) {
+               st.push(node->left);
+           }
+           if (node->right) {
+               st.push(node->right);
+           }
+       }
+       return ans;
+   }
+};*/
+
+class Solution {
+public:
+    int ans = 0;
+    // optimized
+    void zigZag(TreeNode* node, int left, int right) {
         if (!node)
-            return cnt ;
-        cnt++;
-        if (dir == 0) { // last move was left
-            return zigZag(node->right, 1, cnt);
-        } else { // last move was right
-            return zigZag(node->left, 0, cnt);
-        }
+            return;
+
+        ans = max({ans, left, right});
+
+        zigZag(node->left,  right + 1, 0);//last move was left
+        zigZag(node->right, 0, left + 1);//last move was right
     }
     int longestZigZag(TreeNode* root) {
         if (!root)
             return 0;
-        stack<TreeNode*> st;
-        int ans = 0;
-        st.push(root);
-
-        while (!st.empty()) {
-            auto node = st.top();
-            st.pop();
-
-            ans =
-                max({ans, zigZag(node->left, 0, 0), zigZag(node->right, 1, 0)});
-
-            if (node->left) {
-                st.push(node->left);
-            }
-            if (node->right) {
-                st.push(node->right);
-            }
-        }
+        zigZag(root, 0, 0);
         return ans;
     }
 };
