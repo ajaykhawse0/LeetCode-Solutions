b050576a9c4ca1e264cd75be04c683f2bba97d7f
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-07-20 20:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (14.15%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        if(!k)return grid;
+        int m = grid.size();
+        int n = grid[0].size();
+        k = k%(m*n);
+        if(!k)return grid;
+
+        vector<int>arr;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr.push_back(grid[i][j]);
+            }
+        }
+
+        reverse(arr.begin(),arr.end());
+        reverse(arr.begin(),arr.begin()+k);
+        reverse(arr.begin()+k,arr.end());
+
+        int p = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                grid[i][j] = arr[p++];
+            }
+        }
+
+
+    return grid;}
+};
+/*
+Optimal
+
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
        //this problem can solved easily by assuming it as a 1D array and k as the no. of right rotations to be made Then transform to 2D  grid again
        if(k==0)return grid;
         int row = grid.size(); 
@@ -24,3 +57,5 @@ public:
         } 
    return ans; }
 };
+
+*/
