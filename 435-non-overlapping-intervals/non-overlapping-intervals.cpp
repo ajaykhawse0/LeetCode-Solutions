2cfc2ea3bc03ad19757bb15ad44d29d01b2fe7f3
Ajay Khawse <154079458+ajaykhawse0@users.noreply.github.com>
2026-08-16 21:55:04 +0530

    Time: 39 ms (84.88%) | Memory: 93.9 MB (68.88%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        //sort the intervals first for easy traversal
-
-        sort(intervals.begin(),intervals.end(),[&](const vector<int>&a,const vector<int>&b){
-        
-           return a[1]<b[1];
+           sort(intervals.begin(),intervals.end(),[](const vector<int>&a,const vector<int>&b){
+          return a[1]<b[1];
+ 
         });
-        // cout<<intervals[1][0]<<intervals[1][1];
-
-        // then take the first interval as reference;
-        int refStart = intervals[0][0];
-        int refEnd = intervals[0][1];
-
-        int overlappingInt = 0;
+        int overlapping = 0;
+        int first = intervals[0][0];
+        int second = intervals[0][1];
         int n = intervals.size();
-
-        //now iterate over the intervals to find the overlapping ones
         for(int i=1;i<n;i++){
-            // if the start of the interval is lesser than the end of reference Intrval
-            //Then it is an overlapping one 
-           if(intervals[i][0]<refEnd){
-            overlappingInt++;
-           }
-           else{
-            //otherwise change the reference
-            refStart = intervals[i][0];
-            refEnd = intervals[i][1];
-           }
+            if(intervals[i][0] < second){
+                overlapping++;}
+             else{
+        first = intervals[i][0];
+        second = intervals[i][1];
+             }   
         }
-
-        return overlappingInt;
-    }
+   return overlapping; }
 };
